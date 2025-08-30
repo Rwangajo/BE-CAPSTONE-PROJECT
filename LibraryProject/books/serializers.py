d6738d5a6b10df5d5a0e1b281a7341434574e75e
rwangajo <rwangajo>
2025-08-30 12:37:30 -0300

    views in the books app updated

--- LibraryProject/books/serializers.py
+++ LibraryProject/books/serializers.py
@@ -4,4 +4,9 @@ from .models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = '__all__'
+        fields = "__all__"
+
+    def validate_isbn(self, value):
+        if not value.isdigit() or len(value) not in (10, 13):
+            raise serializers.ValidationError("ISBN must be 10 or 13 digits.")
+        return value
--- LibraryProject/books/views.py
+++ LibraryProject/books/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
-
-# Create your views here.
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django.db.models import Q
 from .models import Book
 from .serializers import BookSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.all()
+    queryset = Book.objects.all().order_by("title")
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ["title", "author", "isbn"]
+    ordering_fields = ["title", "author", "published_date"]
 
+    def get_queryset(self):
+        qs = super().get_queryset()
+        available = self.request.query_params.get("available")
+        title = self.request.query_params.get("title")
+        author = self.request.query_params.get("author")
+        isbn = self.request.query_params.get("isbn")
+
+        if available and available.lower() in ("true", "1", "yes"):
+            qs = qs.filter(copies_available__gt=0)
+        if title:
+            qs = qs.filter(title__icontains=title)
+        if author:
+            qs = qs.filter(author__icontains=author)
+        if isbn:
+            qs = qs.filter(isbn__icontains=isbn)
+        return qs
